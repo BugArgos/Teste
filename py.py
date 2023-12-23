4f095abcf87a81911473bd71914370643dc118d7
BugArgos <128261248+BugArgos@users.noreply.github.com>
2023-12-23 03:51:21 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,152 @@
+@media screen and (max-width: 1300px){
+ 
+
+  footer{
+    height: 10vh;
+    width: 100vw;
+    
+  }
+  .resumo-img{
+  border-radius: 7px;
+  height: 130px; /* Ajuste conforme necessário */
+  width: 130px;
+  margin: 10px; /* Espaçamento entre a imagem e os botões */
+  }
+}
+
+@media screen and (max-width: 1150px){
+  
+  .principal{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    height: 75vh ;
+  }
+  .menu-scroll{
+    display: flex;
+    background-color: rgb(144,176 ,147);
+    justify-content: center;
+    align-items: center;
+    border-radius:7px ;
+    width: 64.3vw;
+    height: 100%;
+    padding: 10px;
+  }
+  .info{
+    
+    display: flex;
+    width: 70vw;
+    height: 45vh;
+
+  }
+  body.container {
+    font-family: 'Playfair Display', sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: rgb(216,224,204);
+    height: 100%;
+    width: 100vw;
+    
+   
+  }
+  footer{
+    height: 10vh;
+    width: 100vw;
+    
+  }
+
+  .info2{
+    display: flex;
+    margin: 30px;
+    gap:30px;
+    overflow-y: scroll;
+    overflow: hidden;
+    max-width: 80vw;
+    flex-wrap: wrap;
+    max-height: 60vh ;
+    
+  }
+  .resumo-img{
+  border-radius: 7px;
+  height: 130px; /* Ajuste conforme necessário */
+  width: 130px;
+  margin: 10px; /* Espaçamento entre a imagem e os botões */
+  }
+
+  .resumo{
+    justify-content: center;
+    gap: 20px;
+    padding: 10px;
+    padding-bottom: 30px;
+    padding-top: 0px;
+    display:flex;
+    height: 100%; 
+    width:  80vw;
+    flex-direction: column;
+    
+  
+}
+.titulo{
+  display: flex;
+  justify-content: center;
+  padding-bottom: 25px;
+  border: 2px solid;
+  border-radius: 7px;
+  border-color: white;
+  height: 6vh;
+  width: 36.5vw;
+}
+
+.titulo h1{
+  font-size: 2vw;
+}
+.texto{
+  padding-left: 10px;
+  padding-right: 10px;
+  border: 2px solid;
+  border-radius: 7px;
+  border-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  height: 36vh;
+  width: 35vw;
+  text-align: justify;
+}
+.imagem{
+  margin: 15px;
+  margin-right: 8.8px;
+  border: 2px solid;
+  border-radius: 7px;
+  border-color: white;
+  display:flex;
+  width:  50vw;
+  height: 52vh;
+  flex-direction: column;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+  
+ 
+
+}
+.Unit{
+  display: flex;
+  background-color: rgb(144,176 ,147);
+  width: 100%;
+  min-height: 56vh;
+  border-radius:7px ;
+  align-items: stretch; 
+}
+}
+
+
+@media screen and (max-width: 1150px){
+  
+  .principal{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    height: 75vh ;
+  }
+}
